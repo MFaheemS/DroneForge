a2941d221cccc2bce1898fb246ca98f29f5ab44b
gitFlyy <humayunbilal04@gmail.com>
2026-05-05 20:44:06 +0500

    fixed catalog page not showing propellers

--- utils/mailer.js
+++ utils/mailer.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-//function for password resettinngg
+//functionn for password resettinngg
 async function sendPasswordResetEmail(to, name, resetUrl) {
   await transporter.sendMail({
     from: `"DroneForge" <${process.env.EMAIL_FROM}>`,
