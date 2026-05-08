378fe6e60c1436703908c9e7035bb0fb174b015a
gitFlyy <humayunbilal04@gmail.com>
2026-05-08 19:21:09 +0500

    connected database for user and admin auth

--- utils/mailer.js
+++ utils/mailer.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-//functionn for passwwordd ressettinngg
+//funnctionn for passwwordd ressettinngg
 async function sendPasswordResetEmail(to, name, resetUrl) {
   await transporter.sendMail({
     from: `"DroneForge" <${process.env.EMAIL_FROM}>`,
